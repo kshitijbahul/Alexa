141377b75706d6e91e3f52715c948c13b6e84dc0
KshitijBahul <chunks.3@gmail.com>
2017-05-09 20:52:49 +0530

    Made a conversational Flow

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const Alexa=require('alexa-sdk');
 const APP_ID = 'amzn1.ask.skill.d42a1475-25c6-41c5-93a5-9a4b02c6be65';
 const Messages = require('./telenormessages'); 
 var moment = require('moment');
+const envVariables = require ('./enviornmentVariables');
 
 const handlers = {
     'LaunchRequest' : function(){
-        this.emit(':ask',Messages.Welcome_Message);
+        this.attributes['context'] = 'LaunchRequest';
+        this.attributes['welcomeMessage'] = true;
+        this.emit(`CheckCurrentInternetStatusContextualIntent`);
     },
     'GetYourMaster' : function(){
         console.log(`${Messages.Master_Message} in GetYourMasterIntent the event is ${JSON.stringify(this)}`)
-        this.emit(':ask',Messages.Master_Message);
+        this.emit(':ask',`${Messages.Master_Message} ${Messages.HELP_MSG_PAUSED}`);
     },
     'CheckContextualIntent': function(){
         console.log(`Here in CheckContextualIntent event is ${JSON.stringify(this)}`);
@@ -43,62 +46,65 @@ const handlers = {
     },
     'CheckCurrentBalanceNonContextualIntent': function(){
         console.log(`In  CheckCurrentBalanceNonContextualIntent ${JSON.stringify(this)}`);
-        this.emit(':ask',Messages.CurrentBalanceResponseNonContextual,Messages.HELP_MSG)
+        this.attributes['context'] = 'CheckCurrentBalanceNonContextualIntent';
+        this.emit(':ask',`${Messages.CurrentBalanceResponseNonContextual} `,Messages.HELP_MSG)
     },
     'CheckCurrentUsageNonContextualIntent': function(){
         console.log(`In  CheckCurrentUsageNonContextualIntent ${JSON.stringify(this)}`);
-        this.emit(':ask',Messages.CurrentUsageResponseNonContextual,Messages.HELP_MSG)
+        this.attributes.context ='CheckCurrentUsageNonContextualIntent';
+        this.emit(':ask',`${Messages.CurrentUsageResponseNonContextual} `,Messages.HELP_MSG)
     },
     'CheckCurrentInternetStatusNonContextualIntent': function(){
         console.log(`In  CheckCurrentInternetStatusNonContextualIntent ${JSON.stringify(this)}`);
-        this.emit(':ask',Messages.CurrentInternetStatusResponseNonContextual,Messages.HELP_MSG)
+        this.emit(':ask',`${Messages.CurrentInternetStatusResponseNonContextual} ${Messages.HELP_MSG_PAUSED}`,Messages.HELP_MSG)
     },
     'InvoiceCallbackNonContextualIntent': function(){
         console.log(`In  InvoiceCallbackNonContextualIntent ${JSON.stringify(this)}`);
-        this.emit(':ask',Messages.InvoiceCallbackResponseNonContextual,Messages.HELP_MSG)
+        this.emit(':ask',`${Messages.InvoiceCallbackResponseNonContextual} ${Messages.HELP_MSG_PAUSED}`,Messages.HELP_MSG)
     },
     'CheckCoverageInAreaNonContextualIntent': function(){
         console.log(`In  CheckCoverageInAreaNonContextualIntent ${JSON.stringify(this)}`);
-        this.emit(':ask',Messages.AreaCoverageResponseNonContextual,Messages.HELP_MSG)
+        this.emit(':ask',`${Messages.AreaCoverageResponseNonContextual} ${Messages.HELP_MSG_PAUSED}`,Messages.HELP_MSG)
     },
     'CheckSubscriptionNonContextualIntent': function(){
         console.log(`In  CheckSubscriptionNonContextualIntent ${JSON.stringify(this)}`);
-        this.emit(':ask',Messages.MySubscriptionResponseNonContextual,Messages.HELP_MSG)
+        this.emit(':ask',`${Messages.MySubscriptionResponseNonContextual} ${Messages.HELP_MSG_PAUSED}`,Messages.HELP_MSG)
     },
     'CheckOffersNonContextualIntent': function(){
         console.log(`In  CheckOffersNonContextualIntent ${JSON.stringify(this)}`);
-        this.emit(':ask',Messages.CurrentOffersResponseNonContextual,Messages.HELP_MSG)
+        this.attributes.context ='CheckOffersNonContextualIntent';
+        this.emit(':ask',`${Messages.CurrentOffersResponseNonContextual} ${Messages.HELP_MSG_PAUSED}`,Messages.HELP_MSG)
     },
     'TelenorIdIntent': function(){
         console.log(`In  TelenorIdIntent ${JSON.stringify(this)}`);
         const telenorID = this.event && this.event.request && this.event.request.intent && this.event.request.intent.slots && this.event.request.intent.slots.TelenorId && this.event.request.intent.slots.TelenorId.value;
-        console.log(`In  TelenorIdIntent ${JSON.stringify(telenorID)} and context is ${this.attributes.context} and ${Messages[this.attributes.context]} message is ${Messages[this.attributes.context].message}`);
-        if(this.attributes.context == 'IncreaseBroadBandSpeedFor2Months'){
+        if(!telenorID){
+            this.emit(':ask', `${Messages.NoTelenorIdMentioned}`,`${Messages.NoTelenorIdMentioned}`)
+        }else if (telenorID && telenorID!='123456'){
+            this.emit(':ask', `${Messages.sayAs.digits} ${telenorID} ${Messages.sayAs.sayasEnd} ${Messages.InvalidTelenorId}`,`${Messages.sayAs.digits} ${telenorID} ${Messages.sayAs.sayasEnd} ${Messages.InvalidTelenorId}`)
+        }
+        console.log(`In  TelenorIdIntent ${JSON.stringify(telenorID)} and context is ${this.attributes.context} and ${Messages[this.attributes.context]} message is ${Messages[this.attributes.context]}`);
+        if(this.attributes && this.attributes.context && this.attributes.context == 'IncreaseBroadBandSpeedFor2Months'){
             console.log(`Its IncreaseBroadBandSpeedFor2Months that invoked the telenorIdIntent`);
             console.log(`Its IncreaseBroadBandSpeedFor2Months Checking moment.js ${moment()} and adding months ${moment().add(this.attributes.numberOfMonths,'M')}`);
-            //var newDate = moment().add(this.attributes.numberOfMonths,'M')
             console.log(`${Messages[this.attributes.context]} ${Messages.sayAs.date} ${moment().add(this.attributes.numberOfMonths,'M').format('LL')} ${Messages.sayAs.sayasEnd}`);
-            this.emit(':ask',`${Messages[this.attributes.context]} ${moment().add(this.attributes.numberOfMonths,'M').format('LL')} `,Messages.HELP_MSG);
-        } else if(this.attributes.context == 'IncreaseDataPackBy2GB'){
+            this.emit(':ask',`${Messages[this.attributes.context]} ${moment().add(this.attributes.numberOfMonths,'M').format('LL')} ${Messages.HELP_MSG_PAUSED}`,Messages.HELP_MSG);
+        } else if(this.attributes && this.attributes.context && this.attributes.context == 'IncreaseDataPackBy2GB'){
             console.log(`Its IncreaseDataPackBy2GB that invoked the telenorIdIntent`);
             console.log(`${Messages[this.attributes.context]} ${Messages.sayAs.date} ${moment().add(this.attributes.numberOfMonths,'M').format('LL')} ${Messages.sayAs.sayasEnd}`);
-            this.emit(':ask',`${Messages[this.attributes.context]} ${parseInt(this.attributes['numberOfGigs'])+1.2}GB`,Messages.HELP_MSG);
+            this.emit(':ask',`${Messages[this.attributes.context]} ${parseInt(this.attributes['numberOfGigs'])+1.2}GB ${Messages.HELP_MSG_PAUSED}`,Messages.HELP_MSG);
         }
-        //check fo the telenor id here
-        /*telenorID ==`123456` ? this.attributes && this.attributes.context ? 
-                                this.emit(':tell',Messages[this.attributes.context].message) : this.emit(':tell',Messages.HELP_MSG)
-                             :this.emit(':tell',Messages.TelenorIdNoMatch);*/
-        this.attributes && this.attributes.context ? this.emit(':ask',Messages[this.attributes.context],Messages.HELP_MSG) : this.emit(':ask',Messages.HELP_MSG);
+        this.attributes && this.attributes.context ? this.emit(':ask',`${Messages[this.attributes.context]} ${Messages.HELP_MSG_PAUSED}`,Messages.HELP_MSG) : this.emit(':ask',Messages.HELP_MSG);
     },
     'PostponeInvoiceContextualIntent': function(){
         console.log(`In  PostponeInvoiceContextualIntent ${JSON.stringify(this)}`);
         this.attributes['context'] = 'PostponeInvoice';
-        this.emit(':ask',Messages.RequestTelenorId,Messages.RequestTelenorId);
+        this.emit(':ask',`${Messages.RequestTelenorId}`,Messages.RequestTelenorId);
     },
     'BlockSubscriptionContextualIntent': function(){
         console.log(`In  BlockSubscriptionContextualIntent ${JSON.stringify(this)}`);
         this.attributes['context'] = 'BlockSubscription';
-        this.emit(':ask',Messages.RequestTelenorId,Messages.RequestTelenorId);
+        this.emit(':ask',`${Messages.RequestTelenorId}`,Messages.RequestTelenorId);
     },
     'IncreaseBroadBandSpeedContextualIntent': function(){
         console.log(`In  IncreaseBroadBandSpeedContextualIntent ${JSON.stringify(this)}`);
@@ -123,33 +129,74 @@ const handlers = {
         this.emit(':ask',`${this.attributes['numberOfGigs']}GB ${Messages.DataPackCost} ${Messages.RequestTelenorId}`,`${Messages.DataPackCost} ${Messages.RequestTelenorId}`);
     },
     'CheckCurrentInternetStatusContextualIntent': function(){
+        var welcomeMessage = this.attributes.context == 'LaunchRequest' ? `${Messages.Salutation} ${envVariables.CurrentUser} ${Messages.breaks.twoHundred} `:'';
         console.log(`In  CheckCurrentInternetStatusContextualIntent ${JSON.stringify(this)}`);
         this.attributes['context'] = 'CurrentInternetStatus';
-        this.emit(':ask',Messages.CurrentInternetStatusSMSConfirmation);
+        this.emit(':ask',`${welcomeMessage} ${Messages.CurrentInternetStatusSMSConfirmation}`,`${Messages.CurrentInternetStatusSMSConfirmation}`);
+    },
+    'ContactMeIntent': function () {
+        console.log(`In  ContactMeIntent ${JSON.stringify(this)}`);
+        this.emit(':ask', `${Messages.ContactUserMessage} ${Messages.HELP_MSG_PAUSED}`,Messages.HELP_MSG);
+    },
+    'SkillInformation': function () {
+        console.log(`In  SkillInformation ${JSON.stringify(this)}`);
+        this.emit(':ask', `${Messages.SkillInformation} ${Messages.HELP_MSG_PAUSED}`,Messages.HELP_MSG);
     },
     'AMAZON.YesIntent': function(){
         console.log(`In  AMAZON.YesIntent ${JSON.stringify(this)}`);
-        console.log(`In  AMAZON.YesIntent ${JSON.stringify(this.attributes)} and message is ${Messages[this.attributes.context].message}`);
-        this.attributes && this.attributes.context ? this.emit(':tell',Messages[this.attributes.context],Messages.HELP_MSG) : this.emit(':tell',Messages.HELP_MSG);
+        console.log(`In  AMAZON.YesIntent ${JSON.stringify(this.attributes)} and message is ${Messages[this.attributes.context]}`);
+        console.log(Messages[`${this.attributes.context}YesIntent`]);
+        var message = this.attributes.context && this.attributes.context == 'CurrentInternetStatus' && this.attributes.welcomeMessage 
+                        ? `${Messages[this.attributes.context+"YesIntent"]} ${Messages.breaks['1s']}${Messages.CompressedMessage}` : '';
+        this.attributes.welcomeMessage ? this.attributes.welcomeMessage= false: '';
+        this.attributes.context == 'CurrentInternetStatus' && message ? this.emit(':ask',message) : '' ;
+        this.attributes.context=='CheckCurrentBalanceNonContextualIntent' ? this.emit('CheckCurrentUsageNonContextualIntent') : '' ;
+        this.attributes.context=='CheckCurrentUsageNonContextualIntent' ? this.emit('IncreaseDataPackContextualIntent') : '' ;
+        console.log(`In  AMAZON.YesIntent after !! ${JSON.stringify(this)}`);
+        //this.attributes && this.attributes.context ? this.emit(':tell',Messages[this.attributes.context],Messages.HELP_MSG) : this.emit(':tell',`I don't know what you aare saying yes to. ${Messages.HELP_MSG}`);
+         this.emit(':ask',this.attributes && this.attributes.context ? `${Messages[this.attributes.context+"YesIntent"]} ${Messages.HELP_MSG_PAUSED}`: `I don't know what you are saying yes to. ${Messages.CompressedMessage}`,Messages.HELP_MSG);
     },
     'AMAZON.HelpIntent': function () {
-        console.log(`In  AMAZON.HelpIntent `);
-        const speechOutput = this.t('HELP_MESSAGE');
-        const reprompt = Messages.HELP_MESSAGE;
-        this.emit(':ask', speechOutput, reprompt);
+        console.log(`In  AMAZON.HelpIntent ${JSON.stringify(this)}`);
+        this.emit(':ask', Messages.HELP_MESSAGE);
     },
     'AMAZON.CancelIntent': function () {
         console.log('Came in here in CancelIntent')
         this.emit(':tell', Messages.STOP_MESSAGE);
     },
     'AMAZON.NoIntent' : function(){
-        console.log('Came in here in No Intent')
+        console.log(`Came in here in No Intent ${JSON.stringify(this)}`);
+        console.log('The nointent message is ',Messages[this.attributes.context+'NoIntent']);
+        var message = '';
+        var promptMessage = '';
+        if (this.attributes.NoContext){
+            this.attributes.NoContext = false;
+            this.emit('AMAZON.StopIntent')  ;
+        }
+        this.attributes.context=='CheckCurrentUsageNonContextualIntent' ? this.emit('CheckOffersNonContextualIntent') : '' ;
+        if(this.attributes.context && this.attributes.context == 'CurrentInternetStatus' && this.attributes.welcomeMessage){
+            console.log(`Entered if in No contetnt i.e we are still in welcome flow  ${Messages[this.attributes.context+'NoIntent']} ${Messages.CompressedMessage}`);
+            message = `${Messages[this.attributes.context+'NoIntent']} ${Messages.breaks.twoHundred} ${Messages.CompressedMessage}`;
+            this.attributes.context ='';
+            this.attributes.welcomeMessage = false;
+            //promptMessage = Messages.HELP_MSG;
+            this.emit(':ask',message ,Messages.HELP_MSG);
+        } else if (! Messages[this.attributes.context+'NoIntent']) {
+            console.log(`Here in No Intent could not find the no message for ${this.attributes.context}`);
+            this.attributes['NoContext'] = true;
+            this.emit(':ask',Messages.HELP_MSG,Messages.HELP_MSG);
+        } 
+        else {
+           Messages[this.attributes.context+'NoIntent'] ? this.emit(':ask',`${Messages[this.attributes.context+'NoIntent']} ${Messages.HELP_MSG_PAUSED}` ,Messages.HELP_MSG) 
+                                                              : this.emit('AMAZON.StopIntent')  ;
+        }
     },
     'AMAZON.StopIntent': function () {
-        console.log('Came in here in StopIntent')
-        this.emit(':tell', Messages.STOP_MESSAGE);
+        console.log(`Came in here in StopIntent ${JSON.stringify(this)}`)
+        this.emit(':tell', Messages.STOP_MSG);
     },
     'SessionEndedRequest': function () {
+        console.log('Came in here in SessionEndedRequest')
         this.emit(':tell', Messages.STOP_MESSAGE);
     }
 };
--- telenormessages.js
+++ telenormessages.js
@@ -1,6 +1,12 @@
 `use strict`;
+const moment=require('moment-timezone');
+const _=require('lodash');
+const envVariables=require('./enviornmentVariables');
+const salutation = moment().tz(envVariables.TimeZone).format('HH') <12 ? 'Good Morning' : (moment().format('HH') <16 ? 'Good Afternoon':'Good Evening');
+const salutationEnd = moment().tz(envVariables.TimeZone).format('HH') <12 ? 'Day' : (moment().format('HH') <16 ? 'Afternoon': (moment().format('HH') <20 ? 'Evening':'Night'));
 const sayAs = {
     "telephone": '<say-as interpret-as="telephone">',
+    "digits": '<say-as interpret-as="digits">',
     "date": '<say-as interpret-as="date" format="mdy">',
     "sayasEnd" : '</say-as>',
     "whispered":'<amazon:effect name="whispered">',
@@ -13,31 +19,49 @@ const breaks = {
     "four" :'<break time="4ms"/> ',
     "five" :'<break time="5ms"/> ',
     "eight" :'<break time="8ms"/> ',
+    "ten" :'<break time="10ms"/> ',
+    "fifteen" :'<break time="15ms"/> ',
+    "twenty" :'<break time="20ms"/> ',
+    "fifty" :'<break time="50ms"/> ',
+    "hundred" :'<break time="100ms"/> ',
+    "twoHundred" :'<break time="200ms"/> ',
+    "1s" :'<break time="1s"/> ',
+    "2s" :'<break time="2s"/> ',
+    "3s" :'<break time="3s"/> ',
 };
 module.exports = {
+    breaks,
     sayAs,
+    Salutation : salutation,
+    NoTelenorIdMentioned : `You did not mention a TelenorId, Could you mention the TelenorId again please ?`,
+    InvalidTelenorId: `is not your TelenorId, do you want to try again ?`,
+    CompressedMessage : `I can help you check your balance , usage , Internet Access and Invoices. I can also have someone call you back if you wish, ${breaks.twenty} What can I help you with ?`,
     Welcome_Message :`Welcome to Telenor. I can help you with your Balance, current usage, Internet status. If you want I can also schedule an agent to call you for your invoices`,
     RandomNumberText : `Look what I rolled for you`,
+    HELP_MSG_PAUSED : `${breaks.hundred}Can I help you with anything else ?`,
     HELP_MSG :` Can I help you with anything else ?`,
-    STOP_MSG : `Bye`,
+    STOP_MSG : `Goodbye ,Have a nice ${salutationEnd}`,
     TelenorIdNoMatch :`Sorry you did not mention the correct telenor id`,
     ContextualSuccess:`Sure done .${sayAs.whispered} Kshitij I did the other thing as well ${sayAs.amazonEffectEnd}`,
     Master_Message : `Kshitij will always be the master. Ha ha`,
-    CurrentBalanceResponseNonContextual:`Your current balance for mobile ${sayAs.telephone}90104005 ${sayAs.sayasEnd} is 500 kroner. ${breaks.five} Your current balance for fixed phone ${sayAs.telephone}67105000 ${sayAs.sayasEnd} is 230 kroner`,
-    CurrentUsageResponseNonContextual:`You have now used 1.2 GB of your available 2GB limit for this month`,
+    CurrentBalanceResponseNonContextual:`Your current balance for mobile ${sayAs.telephone}90104005 ${sayAs.sayasEnd} is 500 kroner. ${breaks.five} Your current balance for fixed phone ${sayAs.telephone}67105000 ${sayAs.sayasEnd} is 230 kroner. ${breaks.twenty} Would you want to know your current data Usage`,
+    CurrentUsageResponseNonContextual:`You have now used 1.2 GB of your available 2GB limit for this month ${breaks.fifteen}. Would you want to increase your Datapack`,
     CurrentInternetStatusResponseNonContextual:`We have a registered fault in your area. ${breaks.three} Estimated correction time is today at 14:45`,
     InvoiceCallbackResponseNonContextual:`Our estimated waiting time is 4 minutes. ${breaks.three} Your callback is placed in our queue and you will be contacted by our invoice department shortly`,
     MySubscriptionResponseNonContextual:`You currently are subscribed as ${sayAs.telephone}90104005 ${sayAs.sayasEnd} on a Freedom 2GB plan.${breaks.four}  Based on your current data usage, we recommend upgrading to Freedom 5GB`,
     AreaCoverageResponseNonContextual:`Based on your current location, the best available coverage is 4G+. ${breaks.four}  ${sayAs.whispered}Planned availability of 5G in your area is June 2018 ${sayAs.amazonEffectEnd}`,
-    CurrentOffersResponseNonContextual:`This month at telenor you can`,
+    CurrentOffersResponseNonContextual:`This month at telenor you can Upgrade to 3GB for 299 kroner per month, 15 GB for 499 kroner per Month`,
     CurrentInternetStatusSMSConfirmation:`We have a registered fault in your area. ${breaks.three} Estimated correction time is today at 14:45.  Would you like to receive status updates via SMS?`,
     DataPackCost:`data costs 99 kroner ${breaks.three}`,
-    RequestTelenorId :`Please confirm using your registered 6-digit Telenor ID`,
+    RequestTelenorId :`Please confirm using your registered 6-digit TelenorID`,
     PostponeInvoice :` Invoice with balance 275 kroner is now postponed. New due date is 01 June`,
     BlockSubscription :` Subscription for ${sayAs.telephone}90104005 ${sayAs.sayasEnd} is now temporarily blocked`,
     BroadBandSpeedIncreaseDuration : `How long do you want the BroadBand to be extended for`,
     DataPackIncreaseByMessage : `How may GBs of data do you need`,
     IncreaseBroadBandSpeedFor2Months :` Your broadband speed is now increased from 50Mbit/s to 100Mbit/s until `,
     IncreaseDataPackBy2GB :` Your available data for mobile ${sayAs.telephone}90104005 ${sayAs.sayasEnd} is now`,
-    CurrentInternetStatus :` Status updates will be sent to ${sayAs.telephone}90104005 ${sayAs.sayasEnd}`
+    CurrentInternetStatusYesIntent :` Status updates will be sent to ${sayAs.telephone}90104005 ${sayAs.sayasEnd}`,
+    CurrentInternetStatusNoIntent :``,
+    ContactUserMessage :` I have placed the request and you will be shortly contacted by our customer service team.`,
+    SkillInformation : `This skill has been Built by Miles India`,
 }
